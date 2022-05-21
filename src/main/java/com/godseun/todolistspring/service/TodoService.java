6490079e580f7048ac83eed2b729535e271d5884
godseun_mac <alcy1024@gmail.com>
2022-05-22 00:57:45 +0900

    repository apply service

--- src/main/java/com/godseun/todolistspring/service/TodoService.java
+++ src/main/java/com/godseun/todolistspring/service/TodoService.java
@@ -1,11 +1,21 @@
 package com.godseun.todolistspring.service;
 
+import com.godseun.todolistspring.model.TodoEntity;
+import com.godseun.todolistspring.persistence.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
+  @Autowired
+  private TodoRepository repository;
+
   public String testService() {
-    return "Test Service";
+    TodoEntity entity = TodoEntity.builder().title("first todo item").build();
+    repository.save(entity);
+    TodoEntity saveEntity = repository.findById(entity.getId()).get();
+    return saveEntity.getTitle();
   }
 }
