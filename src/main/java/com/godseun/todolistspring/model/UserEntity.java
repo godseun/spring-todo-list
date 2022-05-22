59eb2167318b0ee906a091f56c09cb0a3e02a845
godseun_mac <alcy1024@gmail.com>
2022-05-22 18:10:19 +0900

    create UserEntity

---
+++ src/main/java/com/godseun/todolistspring/model/UserEntity.java
@@ -0,0 +1,38 @@
+package com.godseun.todolistspring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
+public class UserEntity {
+
+  @Id
+  @GeneratedValue(generator = "system-uuid")
+  @GenericGenerator(name = "system-uuid", strategy = "uuid")
+  private String id;
+
+  @Column(nullable = false)
+  private String userName;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+}
